4beff7d2d9189f1a5682e21457630fb0becf0583
Harsh <valambeharsh1701@gmail.com>
2023-10-15 12:55:48 +0530

    routes established

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signout from './pages/Signout'
+import Profile from './pages/Profile';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signout' element={<Signout/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
