aab06405c1132bce7ed076bbe166506579f0ed75
Harsh <valambeharsh1701@gmail.com>
2023-10-19 22:34:05 +0530

    db initialised

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app=express();
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('connected to db');
+}).catch((err) => {
+    console.error(err);
+});
+const app = express();
 
-app.listen('3000',()=>{
+app.listen('3000', () => {
     console.log('Server is running on port 3000');
 });
+
