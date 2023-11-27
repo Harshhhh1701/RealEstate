c0f1b3b1b8499c5b52366ee76f6a6c2ff5a45880
Harsh <valambeharsh1701@gmail.com>
2023-11-27 23:20:07 +0530

    feat: update user api added integration left;(

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js' 
 import authRouter from './routes/auth.route.js'
-
+import cookieParser from 'cookie-parser';
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('connected to db');
@@ -16,7 +16,7 @@ app.listen('3000', () => {
     console.log('Server is running on port 3000');
 });
 app.use(express.json());
-
+app.use(cookieParser());
 app.get('/', (req, res) => {
     res.send("Hello World");
 })
