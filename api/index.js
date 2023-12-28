5db2b0c56c25ec74886f2eece1138a95ec687253
Harsh <valambeharsh1701@gmail.com>
2023-12-28 21:24:46 +0530

    feat::deploy ready

--- api/index.js
+++ api/index.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js' 
+import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('connected to db');
 }).catch((err) => {
     console.error(err);
 });
+const __dirname = path.resolve();
 const app = express();
 
 app.listen('3000', () => {
@@ -21,14 +23,18 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
     res.send("Hello World");
 })
-app.use("/api/user",userRouter);
-app.use("/api/auth",authRouter);
-app.use("/api/listing",listingRouter);
-app.use((err,req,res,next)=>{
-    const statusCode=err.statusCode||500;
-    const message=err.message||"Internal server error";
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
     return res.status(statusCode).json({
-        success:false,
+        success: false,
         statusCode,
         message,
     });
